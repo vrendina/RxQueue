60dfe8086c011001463ea4489b5c4cb4e1ab0e16
Victor Rendina <vrendina@gmail.com>
2018-08-12 15:54:04 -0400

    Clean build each time

--- gradle/build.sh
+++ gradle/build.sh
@@ -4,4 +4,4 @@ export GRADLE_OPTS=-Xmx1024m
 
 echo 'Branch ['${TRAVIS_BRANCH}']  Tag ['${TRAVIS_TAG}']  Pull Request ['${TRAVIS_PULL_REQUEST}'] Slug ['${TRAVIS_REPO_SLUG}']'
 
-./gradlew build
+./gradlew clean build
