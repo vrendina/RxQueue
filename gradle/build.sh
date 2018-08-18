6822027448620cb81fbc7c37a9835091e9051edd
Victor Rendina <vrendina@gmail.com>
2018-08-18 18:42:19 -0400

    Automatic publishing of releases/snapshots

--- gradle/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export GRADLE_OPTS=-Xmx1024m
-
-echo 'Branch ['${TRAVIS_BRANCH}']  Tag ['${TRAVIS_TAG}']  Pull Request ['${TRAVIS_PULL_REQUEST}'] Slug ['${TRAVIS_REPO_SLUG}']'
-
-./gradlew clean build
